2cad0686abbc697ac9cdcf173b7033484fe52da3
Ian Miller <indmillr@gmail.com>
2023-08-17 14:54:13 -0500

    update

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-import Profile from "@components/profile";
+import Profile from "@components/Profile";
 import { TURBO_TRACE_DEFAULT_MEMORY_LIMIT } from "next/dist/shared/lib/constants";
 
 const MyProfile = () => {
